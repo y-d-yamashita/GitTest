4fd58ac2e1cc9cdbcf59ca7a42467efa0a9f161a
y-yamashita <yamashita.hpcss@gmail.com>
2012-08-31 02:20:58 +0900

    Add main class

--- JavaTestProject/src/YDYamashita.java
+++ JavaTestProject/src/YDYamashita.java
@@ -5,4 +5,9 @@ public class YDYamashita {
 	public String toString(){
 		return "new branch feature-yamashita";
 	}
+	
+	public static void main(String[] args){
+		YDYamashita instance = new YDYamashita();
+		System.out.println(instance.toString());
+	}
 }
